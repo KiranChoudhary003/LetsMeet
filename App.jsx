d5fab0e9eafa70934665d44b57d675b91e6daade
harshygarg04 <hgarg@gmail.com>
2025-04-18 14:35:16 +0530

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+//import EventScreen from "./src/componets/EventScreen";
+//import EventDetailsScreen from './src/componets/EventDetailsScreen';
+import MyEvents from './src/componets/MyEvents';
+
+const App = () => {
+  return (
+      <MyEvents/>
+  )
+}
+
+export default App
