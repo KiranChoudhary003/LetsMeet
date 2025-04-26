8bd698fb0bc899991b06edd97242730508589e8b
Ajay-Jangir <Ajayjangir023@gmail.com>
2025-04-27 00:16:01 +0530

    Finel commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import EventAttend from './src/components/Event Attend';
+// import Connections from './src/components/Connections';
+const App = () => {
+
+  return (
+      <EventAttend />
+  );
+};
+
+export default App;
