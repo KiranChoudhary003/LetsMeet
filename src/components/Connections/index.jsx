d044c6b50dcf3d7d29a9346b1f00a41111fd6214
Ajay-Jangir <Ajayjangir023@gmail.com>
2025-06-04 15:27:52 +0530

    aplha 1

--- src/components/Connections/index.jsx
+++ src/components/Connections/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-// import Entypo from 'react-native-vector-icons/Entypo';
-// import Ionicons from 'react-native-vector-icons/Ionicons';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import {
     Alert,
     Animated,
@@ -282,14 +282,12 @@ const Connections = () => {
                 <View style={styles.searchContainer}>
                     <View >
                         <TouchableOpacity>
-                            <Image source={require('../../assets/arrow.webp')} />
-                            {/* <Ionicons name="arrow-back-outline" size={24} color="black" /> */}
+                            <Ionicons name="arrow-back-outline" size={24} color="black" />
                         </TouchableOpacity>
                     </View>
                     <View style={styles.searchBar}>
                         <View style={styles.lens}>
-                            {/* <Entypo name="magnifying-glass" size={24} color="black" /> */}
-                            <Image source={require('../../assets/magnifying-glass.webp')} />
+                        <Entypo name="magnifying-glass" size={24} color="black" />
                         </View>
 
                         <TextInput
@@ -301,8 +299,7 @@ const Connections = () => {
                         />
                         {search.length > 0 && (
                             <TouchableOpacity onPress={() => setSearch('')}>
-                                {/* <Text style={styles.clearText}><Entypo name="cross" size={24} color="black" /></Text> */}
-                                <Text style = {styles.clearText}>X</Text>
+                                <Text style={styles.clearText}><Entypo name="cross" size={24} color="black" /></Text>
                             </TouchableOpacity>
                         )}
                     </View>
